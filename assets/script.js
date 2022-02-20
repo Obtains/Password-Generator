166bd688e875c56ca40afe8e858c1f94e483a370
Failsafe <hawkhilt@gmail.com>
2022-02-20 11:58:15 -0500

    added input variables

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// Input Variables
+
+var input
+var promptNumber
+var promptCharacter
+var promptUppercase
+var promptLowercase
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
